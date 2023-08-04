061a4ee7b1bfc16644b8fa98c25482c3b7d894d5
Mohab Mohamed Gamal <mohabgamal112233@gmail.com>
2023-08-05 00:10:11 +0300

    Farewell

---
+++ CryptoZombies.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+import "./zombieownership.sol";
+// to give the game a cool name
+contract CryptoZombies is ZombieOwnership
+    {
+
+    }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NFT-Crypto_Zombies-Ethereum-Game
+
+## `Technologies Used:`
+
+- Ethereum
+- Solidity
+- NFT
+- mocha
+- chai 
